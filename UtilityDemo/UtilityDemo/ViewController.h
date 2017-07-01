4002100b314c863b9aca171e1b0d68c2c823298b
nice2meet <ntm@nice2meetdeMacBook-Pro.local>
2017-07-01 16:07:40 +0800

    first commit

---
+++ UtilityDemo/UtilityDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UtilityDemo
+//
+//  Created by nice2meet on 2017/7/1.
+//  Copyright © 2017年 nice2meet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UtilityDemo/UtilityDemoTests/UtilityDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  UtilityDemoTests.m
+//  UtilityDemoTests
+//
+//  Created by nice2meet on 2017/7/1.
+//  Copyright © 2017年 nice2meet. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UtilityDemoTests : XCTestCase
+
+@end
+
+@implementation UtilityDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
